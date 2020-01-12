039bae765348dcbc3b0c8f0ea67b9aea14ca5243
Witold Kupś <witold.kups@interlan.pl>
2020-01-12 18:52:17 +0100

    Add language detection

--- src/app/language/language.service.ts
+++ src/app/language/language.service.ts
@@ -13,7 +13,7 @@ export class LanguageService {
   }
 
   set selectedLanguage(language: string) {
-    if (!this.allowedLanguages.includes(language)) {
+    if (!this.isSupported(language)) {
       this.toast.open(`language '${language}' is not supported`);
       return;
     }
@@ -23,6 +23,61 @@ export class LanguageService {
 
   constructor(private translate: TranslateService, private toast: MatSnackBar) {
     this.allowedLanguages = ["en", "pl"];
-    this.selectedLanguage = this.allowedLanguages[0];
+    this.selectedLanguage = this.getBrowserLanguageIfSupported() || this.allowedLanguages[0];
   }
+
+  private getBrowserLanguageIfSupported() {
+    const browserLanguage = detectBrowserLanguage();
+    if (!browserLanguage) return;
+    const shortLanguage = browserLanguage.substring(0, 2).toLowerCase();
+    if (this.isSupported(shortLanguage))
+      return shortLanguage;
+  }
+
+  private isSupported(language: string) {
+    return this.allowedLanguages.includes(language);
+  }
+
+}
+
+// https://stackoverflow.com/questions/1043339/javascript-for-detecting-browser-language-preference
+function detectBrowserLanguage(): string {
+  let nav = window.navigator,
+    browserLanguagePropertyKeys = ['language', 'browserLanguage', 'systemLanguage', 'userLanguage'],
+    i,
+    language,
+    len,
+    shortLanguage = null;
+
+  // support for HTML 5.1 "navigator.languages"
+  if (Array.isArray(nav.languages)) {
+    for (i = 0; i < nav.languages.length; i++) {
+      language = nav.languages[i];
+      len = language.length;
+      if (!shortLanguage && len) {
+        shortLanguage = language;
+      }
+      if (language && len > 2) {
+        return language;
+      }
+    }
+  }
+
+  // support for other well known properties in browsers
+  for (i = 0; i < browserLanguagePropertyKeys.length; i++) {
+    language = nav[browserLanguagePropertyKeys[i]];
+    //skip this loop iteration if property is null/undefined.  IE11 fix.
+    if (language == null) {
+      continue;
+    }
+    len = language.length;
+    if (!shortLanguage && len) {
+      shortLanguage = language;
+    }
+    if (language && len > 2) {
+      return language;
+    }
+  }
+
+  return shortLanguage;
 }
