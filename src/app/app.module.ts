0521ac6ba91d46361582fde2ef0600669cbe15dc
Witold Kupś <witold.kups@interlan.pl>
2020-01-12 09:05:50 +0100

    Add standard pwa support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ChessViewModule} from './chess-view/chess-view.module';
 import {FormViewModule} from "./form-view/form-view.module";
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        ChessViewModule,
-        FormViewModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ChessViewModule,
+    FormViewModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
