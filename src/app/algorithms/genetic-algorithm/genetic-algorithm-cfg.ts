2aaefd8702de4ec0a114c7659f85e5e9a6927f2c
Witold Kupś <witold.kups@interlan.pl>
2020-01-18 16:43:47 +0100

    Change max strength threshold

--- src/app/algorithms/genetic-algorithm/genetic-algorithm-cfg.ts
+++ src/app/algorithms/genetic-algorithm/genetic-algorithm-cfg.ts
@@ -13,7 +13,7 @@ export const geneticAlgorithmProperties: Properties<GeneticAlgorithmCfg, ConfigV
   outPercentage: {type: ValueType.decimal, current: 0.5, step: 0.01, max: 1, min: 0.01},
   elitism: {type: ValueType.decimal, current: 0.3, step: 0.01, max: 1, min: 0.01},
   maxClassifiers: {type: ValueType.integer, current: 100, step: 1, max: 1, min: 1},
-  strengthThreshold: {type: ValueType.decimal, current: 0.3, step: 0.1, max: 100, min: 0},
+  strengthThreshold: {type: ValueType.decimal, current: 0.3, step: 0.1, max: 5, min: 0},
   mutationProbability: {type: ValueType.decimal, current: 0.3, step: 0.01, max: 1, min: 0.01},
   classifierMutationProbability: {type: ValueType.decimal, current: 0.4, step: 0.01, max: 1, min: 0.01},
 };
