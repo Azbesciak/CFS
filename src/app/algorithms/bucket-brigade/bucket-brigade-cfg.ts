d1f522ba079cbb285ec801f2125b24f4cd6b322a
Witold Kupś <witold.kups@interlan.pl>
2020-01-19 10:45:44 +0100

    Allow max config value to be optional

--- src/app/algorithms/bucket-brigade/bucket-brigade-cfg.ts
+++ src/app/algorithms/bucket-brigade/bucket-brigade-cfg.ts
@@ -11,5 +11,5 @@ export const bucketBrigadeProperties: Properties<BucketBrigadeCfg, ConfigValue> 
   k: {type: ValueType.decimal, current: 0.1, min: 0.01, max: 1, step: 0},
   lifeTax: {type: ValueType.decimal, current: 0.06, min: 0.01, max: 1, step: 0},
   bidTax: {type: ValueType.decimal, current: 0.05, min: 0.01, max: 1, step: 0},
-  winners: {type: ValueType.integer, current: 1, min: 1, max: 100, step: 0}
+  winners: {type: ValueType.integer, current: 1, min: 1, step: 0}
 };
