4058ecde889fa310ae75178ae14517a8e5a10384
Witold Kupś <witold.kups@interlan.pl>
2020-01-19 10:26:41 +0100

    Remove unused maxMsgAge config param

--- src/app/algorithms/bucket-brigade/bucket-brigade-cfg.ts
+++ src/app/algorithms/bucket-brigade/bucket-brigade-cfg.ts
@@ -5,13 +5,11 @@ export interface BucketBrigadeCfg {
     readonly lifeTax: number;
     readonly bidTax: number;
     readonly winners: number;
-    readonly msgAgeThreshold: number;
 }
 
 export const bucketBrigadeProperties: Properties<BucketBrigadeCfg, ConfigValue> = {
   k: {type: ValueType.decimal, current: 0.1, min: 0.01, max: 1, step: 0},
   lifeTax: {type: ValueType.decimal, current: 0.06, min: 0.01, max: 1, step: 0},
   bidTax: {type: ValueType.decimal, current: 0.05, min: 0.01, max: 1, step: 0},
-  winners: {type: ValueType.integer, current: 1, min: 1, max: 100, step: 0},
-  msgAgeThreshold: {type: ValueType.integer, current: 3, min: 0, max: 100, step: 1}
+  winners: {type: ValueType.integer, current: 1, min: 1, max: 100, step: 0}
 };
