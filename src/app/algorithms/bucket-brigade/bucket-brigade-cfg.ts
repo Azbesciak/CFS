102af40d5feda815ec4707c71060f1bd79b18aee
Witold Kupś <witold.kups@interlan.pl>
2020-01-19 11:09:44 +0100

    Fix bucket brigade config

--- src/app/algorithms/bucket-brigade/bucket-brigade-cfg.ts
+++ src/app/algorithms/bucket-brigade/bucket-brigade-cfg.ts
@@ -8,8 +8,8 @@ export interface BucketBrigadeCfg {
 }
 
 export const bucketBrigadeProperties: Properties<BucketBrigadeCfg, ConfigValue> = {
-  k: {type: ValueType.decimal, current: 0.1, min: 0.01, max: 1, step: 0},
-  lifeTax: {type: ValueType.decimal, current: 0.06, min: 0.01, max: 1, step: 0},
-  bidTax: {type: ValueType.decimal, current: 0.05, min: 0.01, max: 1, step: 0},
-  winners: {type: ValueType.integer, current: 1, min: 1, step: 0}
+  k: {type: ValueType.decimal, current: 0.1, min: 0, max: 1, step: 0.01},
+  lifeTax: {type: ValueType.decimal, current: 0.06, min: 0, max: 1, step: 0.01},
+  bidTax: {type: ValueType.decimal, current: 0.05, min: 0, max: 1, step: 0.01},
+  winners: {type: ValueType.integer, current: 1, min: 1, step: 1}
 };
