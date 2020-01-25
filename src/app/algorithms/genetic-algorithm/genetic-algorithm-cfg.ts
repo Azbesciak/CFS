70c2645f378f2f8c1bd52c13a90100ceb8c2fad4
Witold Kupś <witold.kups@interlan.pl>
2020-01-25 17:32:07 +0100

    Rename breeds limit to breeding

--- src/app/algorithms/genetic-algorithm/genetic-algorithm-cfg.ts
+++ src/app/algorithms/genetic-algorithm/genetic-algorithm-cfg.ts
@@ -1,7 +1,7 @@
 import {ConfigValue, Properties, ValueType} from "../field-definition";
 
 export interface GeneticAlgorithmCfg {
-  readonly breedsLimit: number;
+  readonly breedingLimit: number;
   readonly mutationProbability: number;
   readonly classifierMutationProbability: number;
   readonly strengthThreshold: number;
@@ -12,7 +12,7 @@ export interface GeneticAlgorithmCfg {
 
 export const geneticAlgorithmProperties: Properties<GeneticAlgorithmCfg, ConfigValue> = {
   strengthThreshold: {type: ValueType.decimal, current: 0.3, step: 0.1, max: 5, min: 0},
-  breedsLimit: {type: ValueType.integer, current: 1, step: 1, min: 0},
+  breedingLimit: {type: ValueType.integer, current: 1, step: 1, min: 0},
   maxClassifiers: {type: ValueType.integer, current: 100, step: 1, min: 0},
   elitism: {type: ValueType.decimal, current: 0.3, step: 0.01, max: 1, min: 0},
   mutationProbability: {type: ValueType.decimal, current: 0.15, step: 0.01, max: 1, min: 0},
