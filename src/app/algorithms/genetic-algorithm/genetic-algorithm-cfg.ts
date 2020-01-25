79157405962ecdbd5d3d36aa815f82a9039d1518
Witold Kupś <witold.kups@interlan.pl>
2020-01-25 17:31:16 +0100

    Change order of fields and function is GA to stick to execution order

--- src/app/algorithms/genetic-algorithm/genetic-algorithm-cfg.ts
+++ src/app/algorithms/genetic-algorithm/genetic-algorithm-cfg.ts
@@ -11,12 +11,12 @@ export interface GeneticAlgorithmCfg {
 }
 
 export const geneticAlgorithmProperties: Properties<GeneticAlgorithmCfg, ConfigValue> = {
-  breedsLimit: {type: ValueType.integer, current: 1, step: 1, min: 0},
-  outPercentage: {type: ValueType.decimal, current: 0.5, step: 0.01, max: 1, min: 0},
-  elitism: {type: ValueType.decimal, current: 0.3, step: 0.01, max: 1, min: 0},
-  maxClassifiers: {type: ValueType.integer, current: 100, step: 1, min: 0},
   strengthThreshold: {type: ValueType.decimal, current: 0.3, step: 0.1, max: 5, min: 0},
+  breedsLimit: {type: ValueType.integer, current: 1, step: 1, min: 0},
+  maxClassifiers: {type: ValueType.integer, current: 100, step: 1, min: 0},
+  elitism: {type: ValueType.decimal, current: 0.3, step: 0.01, max: 1, min: 0},
   mutationProbability: {type: ValueType.decimal, current: 0.15, step: 0.01, max: 1, min: 0},
   classifierMutationProbability: {type: ValueType.decimal, current: 0.4, step: 0.01, max: 1, min: 0},
+  outPercentage: {type: ValueType.decimal, current: 0.5, step: 0.01, max: 1, min: 0},
 };
 
