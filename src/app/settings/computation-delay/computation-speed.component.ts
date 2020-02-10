1845f2505008d1cc86af93f8649b254c2ffdfcf9
Witold Kupś <witold.kups@interlan.pl>
2020-02-10 21:37:57 +0100

    Replace computation delay with computation speed

--- src/app/settings/computation-delay/computation-speed.component.ts
+++ src/app/settings/computation-delay/computation-speed.component.ts
@@ -5,24 +5,28 @@ import {FormControl} from "@angular/forms";
 import {Unsubscribable} from "rxjs";
 
 @Component({
-  selector: 'app-computation-delay',
-  templateUrl: './computation-delay.component.html',
-  styleUrls: ['./computation-delay.component.scss'],
+  selector: 'app-computation-speed',
+  templateUrl: './computation-speed.component.html',
+  styleUrls: ['./computation-speed.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
-export class ComputationDelayComponent implements OnInit, OnDestroy {
+export class ComputationSpeedComponent implements OnInit, OnDestroy {
   settings = Object.assign({}, environment.computation.delay);
-  control = new FormControl(this.settings.current);
+  control = new FormControl(this.reverseValue(this.settings.current));
   private sub: Unsubscribable;
-  format = (v: number) => `${(v / 1000).toFixed(2)}s`;
 
   constructor(private algorithmService: AlgorithmService) {
   }
 
   ngOnInit() {
     this.algorithmService.updateComputationDelay(this.settings.current);
-    this.sub = this.control.valueChanges.subscribe(v => this.algorithmService.updateComputationDelay(v));
+    this.sub = this.control.valueChanges
+      .subscribe(v => this.algorithmService.updateComputationDelay(this.reverseValue(v)));
+  }
+
+  private reverseValue(value: number) {
+    return this.settings.max - value;
   }
 
   ngOnDestroy(): void {
